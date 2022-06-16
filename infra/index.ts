8ee5fde8333e73946f76d4a4654a62e356c21607
Christian Nunciato <c@nunciato.org>
2022-06-16 14:15:24 -0700

    Update the repo URL

--- infra/index.ts
+++ infra/index.ts
@@ -1,6 +1,7 @@
 import * as pulumi from "@pulumi/pulumi";
 import * as aws from "@pulumi/aws";
 
+// https://us-west-2.console.aws.amazon.com/apprunner/home.
 const connection = new aws.apprunner.Connection("connection", {
     connectionName: "my-connection",
     providerType: "GITHUB",
@@ -15,14 +16,14 @@ const service = new aws.apprunner.Service("service", {
         codeRepository: {
             codeConfiguration: {
                 codeConfigurationValues: {
-                    buildCommand: "npm run build",
+                    buildCommand: "cd app && npm run build",
                     port: "3000",
-                    runtime: "NODEJS_12",
-                    startCommand: "npm start",
+                    runtime: "NODEJS_14",
+                    startCommand: "cd app && npm start",
                 },
                 configurationSource: "API",
             },
-            repositoryUrl: "https://github.com/cnunciato/next-on-aws",
+            repositoryUrl: "https://github.com/cnunciato/pulumi-nextjs-on-aws",
             sourceCodeVersion: {
                 type: "BRANCH",
                 value: "master",
